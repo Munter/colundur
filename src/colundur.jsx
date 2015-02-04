01ef4f827d014df1647d975203b71b1601d4d7fa
Peter Müller <munter@fumle.dk>
2015-02-04 23:23:01 +0100

    First rendering

---
+++ src/colundur.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Colundur = React.createClass({
+  displayName: 'Colundur',
+
+  render: function () {
+    return (
+      <div>This is not a calendar yet</div>
+    );
+  }
+});
+
+module.exports = Colundur;
---
+++ src/init.js
@@ -0,0 +1,4 @@
+var React = require('react');
+var Colundur = require('./colundur.jsx');
+
+React.render(React.createElement(Colundur), document.body);
